40341f2aff3d38b78908e71ba13f9cb334a05e62
Germantru98 <germantruhanov@gmail.com>
2019-05-05 21:15:48 +0400

    JS_1

---
+++ JS/Task1/script.js
@@ -0,0 +1,18 @@
+'use strict';
+var str = prompt("Введите строку")
+var ignore = ["?", "!", ":", ";", ",", ".", " ", "\t", "\r"];
+var letters = {}, result;
+var words = str.split(' ');
+ 
+words.forEach(function (word) {
+    word.split('').forEach(function (letter, i) {
+        if (!letters[letter] && ignore.indexOf(letter) == -1 && -1 != word.indexOf(letter, i + 1)) {
+            letters[letter] = 1;
+        }
+    });
+});
+ 
+result = str.split('').filter(function (v) {
+    return !letters[v];
+}).join('');
+alert(result); //У о был собк
