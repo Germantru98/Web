d10768d926577224f98216496b2b2cc77c90e1cc
Germantru98 <germantruhanov@gmail.com>
2019-05-11 17:22:41 +0400

    Task2

--- JS/Task2/script.js
+++ JS/Task2/script.js
@@ -2,23 +2,27 @@
 var str = prompt("Введите строку");
 str = str.split(/([-,+,/,*])/);
 var res,i;
-for(i = 0;i<str.length;i++)
+for(i = 0;i<=str.length-1;i++)
 {
     if (str[i]==='+')
     {
-        res = parseFloat(i-1)+parseFloat(i+1);
+        str[i+1] = parseFloat(str[i-1])+parseFloat(str[i+1]);
+        res = str[i+1];
     }
     if(str[i]==='-')  
     {
-        res = parseFloat(str[i-1])-parseFloat(str[i+1]);
+        str[i+1] = parseFloat(str[i-1])-parseFloat(str[i+1]);
+        res = str[i+1];
     }
     if(str[i]==='/')  
     {
-        res = parseFloat(i-1)/parseFloat(i+1);
+        str[i+1] = parseFloat(str[i-1])/parseFloat(str[i+1]);
+        res = str[i+1];
     }
     if(str[i]==='*')  
     {
-        res = parseFloat(i-1)*parseFloat(i+1);
-    }
+        str[i+1] = parseFloat(str[i-1])*parseFloat(str[i+1]);
+        res = str[i+1];
+    }    
 }
 alert(res);
