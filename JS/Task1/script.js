f114786a1baf8572da1ee7d125caa0b2f8d66863
Germantru98 <germantruhanov@gmail.com>
2019-05-18 21:52:41 +0400

    18/05/19

--- JS/Task1/script.js
+++ JS/Task1/script.js
@@ -4,23 +4,22 @@ var btn = document.getElementById('btn');
 btn.addEventListener('click', clickHandler);
 
 function clickHandler() {
-    console.log(input.value)
     var str = input.value;
-var ignore = ["?", "!", ":", ";", ",", ".", " ", "\t", "\r"];
-var letters = {}, result;
-var words = str.split(' ');
- 
-words.forEach(function (word) {
-    word.split('').forEach(function (letter, i) {
-        if (!letters[letter] && ignore.indexOf(letter) == -1 && -1 != word.indexOf(letter, i + 1)) {
-            letters[letter] = 1;
-        }
+    var ignore = ["?", "!", ":", ";", ",", ".", " ", "\t", "\r"];
+    var letters = {}, result;
+    var words = str.split(' ');
+
+    words.forEach(function (word) {
+        word.split('').forEach(function (letter, i) {
+            if (!letters[letter] && ignore.indexOf(letter) == -1 && -1 != word.indexOf(letter, i + 1)) {
+                letters[letter] = 1;
+            }
+        });
     });
-});
- 
-result = str.split('').filter(function (v) {
-    return !letters[v];
-}).join('');
-alert(result); //У о был собк
+
+    result = str.split('').filter(function (v) {
+        return !letters[v];
+    }).join('');
+    alert(result); //У о был собк
 }
 
--- JS/Task2/script.js
+++ JS/Task2/script.js
@@ -1,28 +1,29 @@
 'use strict';
-var str = prompt("Введите строку");
-str = str.split(/([-,+,/,*])/);
-var res,i;
-for(i = 0;i<=str.length-1;i++)
-{
-    if (str[i]==='+')
-    {
-        str[i+1] = parseFloat(str[i-1])+parseFloat(str[i+1]);
-        res = str[i+1];
+var input = document.getElementById('txt');
+var btn = document.getElementById('btn');
+btn.addEventListener('click', clickHandler);
+function clickHandler() {
+    var str = input.value;
+    var res, i;
+    str = str.split(/([-,+,/,*])/);
+    for (i = 0; i <= str.length - 1; i++) {
+        if (str[i] === '+') {
+            str[i + 1] = parseFloat(str[i - 1]) + parseFloat(str[i + 1]);
+            res = str[i + 1];
+        }
+        if (str[i] === '-') {
+            str[i + 1] = parseFloat(str[i - 1]) - parseFloat(str[i + 1]);
+            res = str[i + 1];
+        }
+        if (str[i] === '/') {
+            str[i + 1] = parseFloat(str[i - 1]) / parseFloat(str[i + 1]);
+            res = str[i + 1];
+        }
+        if (str[i] === '*') {
+            str[i + 1] = parseFloat(str[i - 1]) * parseFloat(str[i + 1]);
+            res = str[i + 1];
+        }
     }
-    if(str[i]==='-')  
-    {
-        str[i+1] = parseFloat(str[i-1])-parseFloat(str[i+1]);
-        res = str[i+1];
-    }
-    if(str[i]==='/')  
-    {
-        str[i+1] = parseFloat(str[i-1])/parseFloat(str[i+1]);
-        res = str[i+1];
-    }
-    if(str[i]==='*')  
-    {
-        str[i+1] = parseFloat(str[i-1])*parseFloat(str[i+1]);
-        res = str[i+1];
-    }    
+    alert(res);
 }
-alert(res);
+
